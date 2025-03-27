c5bb737a0d87e963579cbe4d4a3a66280a245d1a
kaua alves queiros <89699012+kaua-alves-queiros@users.noreply.github.com>
2025-03-27 14:28:50 -0400

    created shared types

---
+++ swagger-definitions/utils.js
@@ -0,0 +1,14 @@
+const ErrorResponse = {
+    success: false,
+    error: 'Some server error'
+}
+
+const NotFoundResponse = {
+    success: false,
+    error: 'Some server error'
+}
+
+module.exports = {
+    ErrorResponse,
+    NotFoundResponse,
+}
